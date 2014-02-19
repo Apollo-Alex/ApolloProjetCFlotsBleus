6dea765da10b3ba7feb786aeb6bf78ed264cd212
Apollo-Alex <alexandregilles1993@hotmail.com>
2014-02-19 10:57:00 +0100

    Projet C - CodeBlock

---
+++ main.c
@@ -0,0 +1,13 @@
+/*******************************
+    *   @autheur:   GILLES A.
+    *   @projet:    Projet C - Les Flots Bleus
+    *   @date:      2014
+********************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    return(0);
+}
